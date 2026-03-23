ef2feec581c35c201a1ca8222ef83551d41a5932
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2026-03-23 15:53:54 -0600

    [main] manejo de df/lf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+// Importa las herramientas para el Lazy API.
+// El modo Lazy construye un plan de consulta
+// y solo lo ejecuta cuando llamas a .collect()
+use polars::lazy::prelude::*;
+// Importa los tipos básicos necesarios, como Series,
+// ChunkedArray y funciones de agregación.
+use polars::prelude::*;
+
+// simpliciación de los tipos de dato para la aplicación
+pub type DataFrame = polars::frame::DataFrame;
+pub type PolarsResult<T> = Result<T, polars::error::PolarsError>;
+
+/**
+ * Función para generar un DF a partir de un archivo .csv
+ */
+pub fn load_csv(path: &str) -> PolarsResult<DataFrame> {
+    let file = std::fs::File::open(path).unwrap();
+    CsvReader::new(file).finish()
+}
+
+/**
+ * Función principal
+ */
 fn main() {
     println!("Hello, world!");
+
+    let df = load_csv("src/data/data.csv").unwrap();
+    let lf = df.clone().lazy();
+
+    let data: PolarsResult<DataFrame> = lf
+        .select([col("Extension"), col("Queue")])
+        .sort(["Queue"], Default::default())
+        .collect();
+
+    let ans = data.unwrap();
+    println!("\n=== Polars ===");
+    println!("{}", ans);
 }
