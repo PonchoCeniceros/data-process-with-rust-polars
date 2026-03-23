e03575262a6900e5403d908ee422d924f67ccdc1
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2026-03-23 16:46:34 -0600

    [main] mejorando un poco el codigo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use colored::*;
+
 // Importa las herramientas para el Lazy API.
 // El modo Lazy construye un plan de consulta
 // y solo lo ejecuta cuando llamas a .collect()
@@ -7,6 +9,7 @@ use polars::lazy::prelude::*;
 use polars::prelude::*;
 
 // simpliciación de los tipos de dato para la aplicación
+pub type StdFile = std::fs::File;
 pub type DataFrame = polars::frame::DataFrame;
 pub type PolarsResult<T> = Result<T, polars::error::PolarsError>;
 
@@ -14,25 +17,41 @@ pub type PolarsResult<T> = Result<T, polars::error::PolarsError>;
  * Función para generar un DF a partir de un archivo .csv
  */
 pub fn load_csv(path: &str) -> PolarsResult<DataFrame> {
-    let file = std::fs::File::open(path).unwrap();
-    CsvReader::new(file).finish()
+    // estoy obteniendo mi archivo csv a traves de la monada Result
+    let file_resl = StdFile::open(path);
+    // resuelvo el contenido de mi monada
+    match file_resl {
+        Ok(file) => CsvReader::new(file).finish(),
+        Err(_) => Ok(DataFrame::empty()),
+    }
+}
+
+/**
+ * Función que me regresa texto coloreado
+ */
+fn get_banners() -> (ColoredString, ColoredString) {
+    let header = "== Hello world =="
+        .black()
+        .bold()
+        .italic()
+        .on_bright_green();
+    let banner = "\n==    Polars   ==".black().bold().on_blue();
+    (header, banner)
 }
 
 /**
  * Función principal
  */
 fn main() {
-    println!("Hello, world!");
-
-    let df = load_csv("src/data/data.csv").unwrap();
-    let lf = df.clone().lazy();
-
+    let (header, banner) = get_banners();
+    println!("{}", header);
+    let df = load_csv("src/data/data.csv").unwrap_or(DataFrame::empty());
+    let lf = df.lazy();
     let data: PolarsResult<DataFrame> = lf
         .select([col("Extension"), col("Queue")])
         .sort(["Queue"], Default::default())
         .collect();
-
-    let ans = data.unwrap();
-    println!("\n=== Polars ===");
+    let ans = data.unwrap_or(DataFrame::empty());
+    println!("{}", banner);
     println!("{}", ans);
 }
